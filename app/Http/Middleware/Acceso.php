a6cab09e8ab19cd6824b559ddb9fa9af4e174882
jonhks <jonathan@laboratoria.la>
2018-05-30 10:08:02 -0500

    usando un api con react

---
+++ app/Http/Middleware/Acceso.php
@@ -0,0 +1,56 @@
+<?php
+
+// namespace App\Http\Middleware;
+
+// use Closure;
+
+// class Acceso
+// {
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @param  \Closure  $next
+//      * @return mixed
+//      */
+//     public function handle($request, Closure $next)
+//     {
+//         return $next($request);
+//     }
+// }
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel {
+
+	/**
+	 * The application's global HTTP middleware stack.
+	 *
+	 * @var array
+	 */
+	// Aquí se programa el Middleware que se cargará siempre y globalmente en la aplicación.
+	protected $middleware = [
+		'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
+		'Illuminate\Cookie\Middleware\EncryptCookies',
+		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
+		'Illuminate\Session\Middleware\StartSession',
+		'Illuminate\View\Middleware\ShareErrorsFromSession',
+		'App\Http\Middleware\VerifyCsrfToken',
+	];
+
+	/**
+	 * The application's route middleware.
+	 *
+	 * @var array
+	 */
+
+	// Aquí se programa el Middleware que se cargará dependiendo de la ruta.
+	protected $routeMiddleware = [
+		'auth' => 'App\Http\Middleware\Authenticate',
+		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
+		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
+	];
+
+}
